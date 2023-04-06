9dad3b2ac9d61b6b682410bf8fc47b0114c1a6cb
this-is-spear <rjsckdd12@gmail.com>
2023-04-06 19:49:55 +0900

    Chore : 추가 프로젝트 구성
    ###Description###
    - 추가(project) : 멀티 모듈 프로젝트 구성
    - 추가(core) : 공통 모듈 구성
    - 추가(executor) : 코드 실행 기능 구성
    - 테스트(hello) : hello 테스트 추가

---
+++ core/src/main/java/core/entity/Hello.java
@@ -0,0 +1,7 @@
+package core.entity;
+
+public final class Hello {
+	public static String hello() {
+		return "Hello";
+	}
+}
---
+++ executor/src/test/java/code/ui/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package code.ui;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureWebMvc
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+	private static final String RESPONSE = "Hello";
+	@Autowired
+	protected MockMvc mockMvc;
+
+	@Test
+	void sayHello() throws Exception {
+		mockMvc.perform(get("/hello"))
+			.andDo(print())
+			.andExpect(content().string(RESPONSE))
+			.andExpect(status().isOk());
+	}
+}
