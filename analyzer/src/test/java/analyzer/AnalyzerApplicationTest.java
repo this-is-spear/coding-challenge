c656c9b5aeff857ce44cdf3b4c75a3a0447bf137
this-is-spear <rjsckdd12@gmail.com>
2023-04-07 16:01:34 +0900

    Chore : 추가 analyzer 프로젝트 구성
    ###Description###
    - 추가(api) : analyzer 구성
    - 추가(hello) : hello API 추가
    - 테스트(hello) : hello 테스트 추가

---
+++ analyzer/src/test/java/analyzer/AnalyzerApplicationTest.java
@@ -0,0 +1,11 @@
+package analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzerApplicationTest {
+	@Test
+	void contextLoads() {
+	}
+}
