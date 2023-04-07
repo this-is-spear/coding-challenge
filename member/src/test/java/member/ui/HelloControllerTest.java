3b34b4ae451dd2ba2132b147d5a73b00f9d74bba
this-is-spear <rjsckdd12@gmail.com>
2023-04-08 00:46:13 +0900

    Chore : 추가 member 프로젝트 구성
    ###Description###
    - 추가(api) : member 구성
    - 추가(hello) : hello API 추가
    - 테스트(hello) : hello 테스트 추가

---
+++ member/src/test/java/member/ui/HelloControllerTest.java
@@ -0,0 +1,25 @@
+package member.ui;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureWebMvc
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+	private static final String RESPONSE = "Hello";
+	@Autowired
+	protected MockMvc mockMvc;
+
+	@Test
+	void sayHello() throws Exception {
+		mockMvc.perform(get("/hello"))
+			.andExpect(content().string(RESPONSE))
+			.andExpect(status().isOk());
+	}
+}
